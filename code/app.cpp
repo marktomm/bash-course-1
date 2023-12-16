e7081cbc8d502e4db9c6915ef858932b8cec11f7
Mark Tomm <mark.tomm@martem.ee>
2023-12-16 10:27:24 +0200

    commit stuff

--- code/app.cpp
+++ code/app.cpp
@@ -1,41 +1,70 @@
-#include <iostream>
 #include <vector> // array like container
+// #include <algorithm>
+// #include <iterator>
+
 #include <string>
-#include <algorithm>
-#include <iterator>
+#include <iostream>
 #include <fstream>
 
+// class MyType {
+// public:
+//     MyType() {
+//         a = 10;
+//         str_ = "GG";
+//     }
+
+//     MyType(int a) {
+//         a = a;
+//     }
+
+//     MyType(std::string s) {
+//         str_ = s;
+//     }
+
+//     void fn() {
+//         std::cout << a << ": " << str_ << "\n";
+//     }
+
+//     int a{5};
+//     std::string str_;
+// };
+
 int main(int argc, char *argv[]) {
-    std::vector<std::string> arguments;
-    std::copy(argv, argv + argc, 
-        std::back_inserter(arguments));
+    // MyType x1;
+    // x1.fn();
+    // MyType x2(222);
+    // std::string myData = "have some text";
+    // std::cout << myData;
+    // otherFn(myData);
+    // `file_h` is a handle to the resource that is the open file
+    // std::ifstream file_h("./app.conf"); // OOP. call the ctor.
+    // std::vector<std::string> var_arr;
+    // if(file_h.is_open()) {
+    //     // std::cout << "some text" << var;
+    //     std::string var;
+    //     // file_h >> var;
+    //     // char c_v = file_h.get();
+    //     while(!file_h.eof()) {
+    //         std::getline(file_h, var);
+    //         // std::cout << var << '\n';
+    //         var_arr.push_back(var);
+    //     }
+    // }
 
-    std::cout << arguments.size() << "\n";
+    // for(auto iter : var_arr) {
+    //     // std::cout << iter << "\n";
+    // }
 
-    arguments.push_back("--more-args");
+    std::vector<int> vec = { 1, 2, 3, 4, 5 };
+    vec.push_back(6);
+    vec.pop_back();
 
-    // memory is allocated for the object in memory
-    // and the constructor is called
-    std::ifstream file("./app.conf");
-    if(file.is_open()) {
-        char c = file.get();
-        std::string ts;
-        while(file.good()) {
-            if(c == '\n') {
-                arguments.push_back(ts);
-                ts = "";
-            } else {
-                ts += c;
-            }
-            c = file.get();
-        }
-        if(ts.size()) {
-            arguments.push_back(ts);
-        }
+    for(auto elem : vec) {
+        std::cout << elem << "\n";
     }
 
-    int i = 1;
-    for(auto elem : arguments) {
-        std::cout << "arg " << i++ << ": " << elem << "\n";
-    }
+    std::cout << vec[0] << "\n";
+
+    // std::ofstream ofile_h("./hhh");
+    return 0;
 }
